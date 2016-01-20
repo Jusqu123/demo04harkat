e74958a276ad79cb3f9b1b7ca9dc1a145d7bb0a4
Jusqu123 <jusquu123@gmail.com>
2016-01-20 15:38:49 +0200

    end of the day

---
+++ demo4tehtävä1/Elevator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo4tehtävä1
+{
+    class Elevator
+    {
+
+        //Max floor
+        private readonly int maxFloor = 5;
+        private readonly int MinFloor = 0;
+        //member field
+        private int floor; // huom floor on pienellä
+        // property
+        public int Floor
+        {
+            get
+            {
+                return floor;
+            }
+            set
+            {
+                if (value <= maxFloor) floor = value;
+                else
+                {
+                    Console.WriteLine("out of bounds you rebel bastard");
+                    floor = maxFloor;
+                }
+            }
+        }
+    }
+}
+            
+        
+
---
+++ demo4tehtävä1/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace demo4tehtävä1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elevator elevator = new Elevator();
+                    int number = 0;
+            elevator.Floor = number;
+            Console.Write("Give a floor number > ");
+            string line = Console.ReadLine();
+
+        
+
+            do
+            {
+                Console.WriteLine("Number was " + number);
+                bool result = int.TryParse(line, out number);
+
+
+
+                Console.WriteLine("Number was " + number);
+
+         
+                Console.WriteLine("*Elevator music* going to floor : " + elevator.Floor);
+                Console.ReadLine();
+                Console.WriteLine("Floor number is now " + number);
+            } while (number != -1);
+
+        }
+    }
+}
+
+
+
+
+//do {
+// kysy kerros
+//siirrä hissiä jos mahdollista
+//tulosta missä hissi on
+
+
+
+//}while (floor != 0)
